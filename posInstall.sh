c963eb87c092410aa6ad7c73f492f3111dfc7865
Thiago <novaesthago@gmail.com>
2020-02-12 07:56:14 -0300

    Criação de Diretórios padrão de uso

--- posInstall.sh
+++ posInstall.sh
@@ -8,6 +8,14 @@ mkdir /home/$USER/Downloads/programas
 
 cd /home/$USER/Downloads/programas
 
+## Diretórios Padrão de Uso ##
+mkdir /home/$USER/Documentos/Academico
+
+mkdir /home/$USER/Documentos/Work
+
+mkdir /home/$USER/Documentos/Pessoal
+mkdir /home/$USER/Documentos/Pessoal/Repositórios
+
 ## Arquivos .deb ##
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget -c https://az764295.vo.msecnd.net/stable/f359dd69833dd8800b54d458f6d37ab7c78df520/code_1.40.2-1574694120_amd64.deb
