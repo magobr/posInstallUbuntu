689b0f61aedbba1f4fa3b32e5a3254f7024eb01f
Thiago <novaesthago@gmail.com>
2020-03-23 17:10:08 -0300

    Adição de pacotes e repositórios

--- posInstall.sh
+++ posInstall.sh
@@ -2,16 +2,25 @@
 
 sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock
 
+## Atualizar S.0 ##
+sudo apt upgrade -y
+
+## Aualizar Pacotes ##
 sudo apt-get update
 
-mkdir /home/$USER/Downloads/programas
+sudo apt install flatpak -y
 
+mkdir /home/$USER/Downloads/programas
 cd /home/$USER/Downloads/programas
 
 ## Diretórios Padrão de Uso ##
 mkdir /home/$USER/Documentos/Academico
 
 mkdir /home/$USER/Documentos/Work
+
+mkdir /home/$USER/Documentos/Church
+mkdir /home/$USER/Documentos/Church/Jovens
+mkdir /home/$USER/Documentos/Church/Adolecentes
 
 mkdir /home/$USER/Documentos/Pessoal
 mkdir /home/$USER/Documentos/Pessoal/Repositórios
@@ -21,22 +30,22 @@ wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 wget -c https://az764295.vo.msecnd.net/stable/f359dd69833dd8800b54d458f6d37ab7c78df520/code_1.40.2-1574694120_amd64.deb
 
 ## install Gerenciador de pacotes ##
-sudo apt install snap
-sudo apt install curl
+sudo apt install snap -y
+sudo apt install curl -y
 
 ## install Apps (dpkg) ##
-sudo dpkg -i *.deb
+sudo dpkg -i *.deb -y
 
 ## install Apps ##
-sudo apt install vim
-sudo apt install gnome-tweaks
+sudo apt install vim -y
+sudo apt install gnome-tweaks -y
 
 ## intall Git ##
-sudo apt install git
+sudo apt install git -y
 
 ## install NodeJs ##
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install nodejs -y
+sudo apt install npm -y
 sudo npm install yarn --global
 
 ## ReactJs ##
@@ -46,12 +55,6 @@ sudo npm install -g create-react-app
 npm install -g expo-cli
 
 ## install php ##
-sudo apt install apache2 
-sudo apt install php
+sudo apt install apache2 -y 
+sudo apt install php -y
 
-## install Apps (Snap) ##
-sudo snap install blix
-sudo snap install libreoffice
-
-## Atualizar S.0 ##
-sudo apt upgrade
