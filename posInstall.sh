959665405425bddf1a3a989831a36df58d649aff
Thiago <novaesthago@gmail.com>
2020-04-15 09:46:12 -0300

    Ajustes pre teste

--- posInstall.sh
+++ posInstall.sh
@@ -6,7 +6,9 @@ sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock
 sudo apt upgrade -y
 
 ## Aualizar Pacotes ##
-sudo apt-get update
+sudo apt-get update -y
+
+sudo dpkg --configure -a
 
 sudo apt install flatpak -y
 
@@ -26,9 +28,6 @@ mkdir /home/$USER/Documentos/Pessoal/Repositórios
 mkdir /home/$USER/Downloads/programas
 cd /home/$USER/Downloads/programas
 
-## pCloud ##
-wget -c https://p-def1.pcloud.com/cBZLReAUjZLbsoWfZZZNTY9a7Z2ZZ7uRZkZpOo5VZzFZtkZO0ZYXZ45Z37ZpVZY0Z3FZr5ZGFZh5ZVXZyVZtouTkZE8jKR0KmdU0kUeWQcx9OBfW0LDmk/pcloud
-
 ## Arquivos .deb ##
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget -c https://az764295.vo.msecnd.net/stable/f359dd69833dd8800b54d458f6d37ab7c78df520/code_1.40.2-1574694120_amd64.deb
@@ -45,10 +44,10 @@ sudo apt install flatpak -y
 sudo apt install vim -y
 sudo apt install gnome-tweaks -y
 sudo apt-get install youtube-dl -y
-flatpak run org.mozilla.Thunderbird
+sudo snap install thunderbird -y
 
 ## install Docker ##
-snap install docker -y
+sudo snap install docker -y
 
 ## install Vagrant ##
 sudo apt-get install vagrant -y
@@ -60,14 +59,4 @@ sudo apt install git -y
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo npm install yarn --global
-
-## ReactJs ##
-sudo npm install -g create-react-app
-
-## React Native ##
-npm install -g expo-cli
-
-## install php ##
-sudo apt install apache2 -y 
-sudo apt install php -y
 
